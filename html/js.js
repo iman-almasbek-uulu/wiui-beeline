218ef564145131aece21b04869da6dfec24098a6
almasbek uulu iman <iman.almasbekuulu.kk@gmail.com>
2022-01-24 17:31:16 +0300

    first commit

--- html/js.js
+++ html/js.js
@@ -1,1 +1,14 @@
-const star = document.querySelector(data)
+"use strict";
+    // исполбзование строго режима
+    let obj = {
+        name: "iman",
+        age: 18,
+        from: "Kyrgyzstan"
+    };
+
+    console.log(obj.from);
+
+
+    // Массивы создается с помощью квадратных скобок
+    let arr = ["apple", "iphone", "pen"];
+    console.log(arr[2]);
